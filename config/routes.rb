f2b59b7bf8da24172db493fdbe50f4d64d5f3fc4
Jaime Montes de Oca <jaime0422@live.com>
2022-09-08 21:51:59 -0700

    Autologin feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
     get "/me", to: "users#show"
     post "/signup", to: "users#create"
+    # post "/login", to: "users#show"
 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
