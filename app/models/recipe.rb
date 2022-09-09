4e46749db109d37ffc99b825ae86de544f6f774c
Jaime Montes de Oca <jaime0422@live.com>
2022-09-08 20:53:35 -0700

    Models working and passing tests

---
+++ app/models/recipe.rb
@@ -0,0 +1,8 @@
+class Recipe < ApplicationRecord
+
+    validates :title, presence: true
+    validates :instructions, length: {minimum: 50}
+
+    belongs_to :user
+    
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+    validates :username, presence: true, uniqueness: true
+
+    has_many :recipes
+
+    has_secure_password
+
+end
